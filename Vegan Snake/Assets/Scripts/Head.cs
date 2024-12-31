724435c7a305c7b118cbc83404144dd2412a4af5
wesleysotnas64 <wesleysotnas64@gmail.com>
2024-12-31 11:08:02 -0300

    feat: Head Movimentation.

---
+++ Vegan Snake/Assets/Scripts/Head.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour
+{
+    public Vector3 direction;
+    public Vector3 lastDirection;
+    public Vector3 lastPosition;
+    public float moveTime;
+    public float currentTime;
+    void Start()
+    {
+        currentTime = 0.0f;
+    }
+
+    void Update()
+    {
+        SelectDirection();
+        Move();
+    }
+
+    public void SelectDirection()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            direction = new Vector3(0, 1, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            direction = new Vector3(0, -1, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            direction = new Vector3(1, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            direction = new Vector3(-1, 0, 0);
+        }
+    }
+
+    public void Move()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime > moveTime)
+        {
+            if(direction == -lastDirection)
+            {
+                direction *= -1;
+            }
+
+            currentTime = 0;
+            lastDirection = direction;
+            lastPosition = transform.position;
+            transform.position += direction;
+            transform.up = direction;
+            
+        }
+    }
+}
