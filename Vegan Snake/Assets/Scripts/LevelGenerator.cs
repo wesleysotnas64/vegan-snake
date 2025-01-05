e589ab6959d025d733c3b620650dd12cfc8f1413
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-01-05 13:17:01 -0300

    feat: Add Zzzzz prefab. When Snake Sleep.

--- Vegan Snake/Assets/Scripts/LevelGenerator.cs
+++ Vegan Snake/Assets/Scripts/LevelGenerator.cs
@@ -8,16 +8,29 @@ public class LevelGenerator : MonoBehaviour
 
     void Start()
     {
-        GenerateHorizontalLineBushWall(-10, 10,  8);
-        GenerateHorizontalLineBushWall(-10, 10, -8);
+        TestLevel();
     }
 
+    private void TestLevel()
+    {
+        GenerateHorizontalLineBushWall(-10, 10,  8);
+        GenerateHorizontalLineBushWall(-10, 10, -8);
+        GenerateVerticalLineBushWall(-7, 7, -10);
+        GenerateVerticalLineBushWall(-7, 7, 10);
+    }
 
     public void GenerateHorizontalLineBushWall(int initX, int finalX, int heightY)
     {
         for(int x = initX; x <= finalX; x++)
         {
             Instantiate(bush).transform.position = new Vector3(x, heightY, 0);
+        }
+    }
+    public void GenerateVerticalLineBushWall(int initY, int finalY, int distX)
+    {
+        for(int y = initY; y <= finalY; y++)
+        {
+            Instantiate(bush).transform.position = new Vector3(distX, y, 0);
         }
     }
 }
